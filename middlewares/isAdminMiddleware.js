74e5699a28bd8b17f00edf5b2f6d6c1bdc7f0b67
amir <amirnobari1990@gmail.com>
2024-05-20 18:31:26 +0330

    chore : "update codeditor format"

--- middlewares/isAdminMiddleware.js
+++ middlewares/isAdminMiddleware.js
@@ -1,45 +1,37 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-const isAdminMiddleware = async (req, res, next) =>
-{
-    try
-    {
-        const authHeader = req.headers['authorization']
-        const token = authHeader && authHeader.split(' ')[1]
+const isAdminMiddleware = async (req, res, next) => {
+	try {
+		const authHeader = req.headers['authorization']
+		const token = authHeader && authHeader.split(' ')[1]
 
-        if (!token)
-        {
-            return res.status(401).json({ error: 'Authorization header is missing' })
-        }
+		if (!token) {
+			return res.status(401).json({error: 'Authorization header is missing'})
+		}
 
-        try
-        {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            // بررسی معتبر بودن توکن
-            const user = await User.findById(decoded.userId)
-            if (!user)
-            {
-                return res.status(401).json({ error: 'Invalid token' })
-            }
+			// بررسی معتبر بودن توکن
+			const user = await User.findById(decoded.userId)
+			if (!user) {
+				return res.status(401).json({error: 'Invalid token'})
+			}
 
-            // بررسی فیلد isAdmin از دیتابیس
-            if (!user.isAdmin)
-            {
-                return res.status(403).json({ error: 'You do not have permission to access this route' })
-            }
-        } catch (error)
-        {
-            return res.status(401).json({ error: 'Invalid token signature' })
-        }
+			// بررسی فیلد isAdmin از دیتابیس
+			if (!user.isAdmin) {
+				return res.status(403).json({error: 'You do not have permission to access this route'})
+			}
+		} catch (error) {
+			return res.status(401).json({error: 'Invalid token signature'})
+		}
 
-        next()
-    } catch (error)
-    {
-        console.error(error)
-        res.status(500).json({ error: 'Internal server error' })
-    }
+		next()
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({error: 'Internal server error'})
+	}
 }
 
 module.exports = isAdminMiddleware
